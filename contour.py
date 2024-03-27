ca6d9064e536504e60cdf1c17d7b796a86546ff6
egrozell <ethan.grozell@ucalgary.ca>
2024-03-27 10:22:57 -0600

    made plot creation a function so multiple plots can be made

--- contour.py
+++ contour.py
@@ -4,18 +4,24 @@ import pandas as pd
 import numpy as np 
 
 def main():
-# Creating 2-D grid of features 
-    contour_data = pd.read_csv("data.txt")
+    plot_contour("horizontal_contour_g3.csv",19.5)
+def plot_contour(file,depth):
+    """
+    takes file name that should be in csv format x, y, z with z being a value at x and y and plots a contour
+    first row should have x,y,z
+    """
+    depth = str(depth)
+    contour_data = pd.read_csv(file)
     Z = contour_data.pivot_table(index='x',columns='y',values='z').T.values
     X_u = np.sort(contour_data.x.unique())
     Y_u = np.sort(contour_data.y.unique())
     X,Y = np.meshgrid(X_u,Y_u)
     fig, ax = plt.subplots(1, 1) 
     ax.contourf(X, Y, Z) 
-      
-    ax.set_title('Contour Plot') 
+    fig_name = file[0:-4]      
+    ax.set_title('Contour Plot At Depth: '+depth+' [cm] \n'+fig_name) 
     ax.set_xlabel('x [cm]') 
     ax.set_ylabel('y [cm]') 
-    plt.savefig("group3contour")
+    plt.savefig(fig_name)
 if __name__ == "__main__":
     main()
